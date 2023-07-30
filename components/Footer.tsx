196822d6720aac525eefe705b3477117a300cfb9
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-07-29 23:36:37 -0500

    Added Footer Component
    Desktop version

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { MobileContext } from "@/contexts/MobileContext"
+import { useContext } from "react"
+
+export default function Footer(){
+  const isMobile = useContext(MobileContext);
+  const footer = isMobile ? <></> :
+  <footer className="flex flex-row justify-between bg-primary dark:bg-primary-dark items-center p-2">
+    <a className='pl-2 text-2xl overflow-hidden' href="https://www.imsa.edu/academics/academic-support-services/">IMSA Academic Support Services</a>
+    <a href="https://github.com/Vinceyou1/IMSATutoringV2" className="bg-[url(/github-mark.svg)] dark:bg-[url(/github-mark-white.svg)] bg-contain"><img src='/github-mark.svg' className="invisible w-[3.075rem] h-12"></img></a>
+  </footer>
+  return footer
+}
