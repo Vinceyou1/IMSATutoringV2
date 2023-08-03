448b22c2735021a72efc5bdcb380d752ceeec242
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-08-02 21:41:46 -0500

    Added loading status to user context

--- contexts/UserContext.tsx
+++ contexts/UserContext.tsx
@@ -4,23 +4,32 @@ import React from "react";
 import { createContext } from "react";
 import { FirebaseAuthContext } from "./FirebaseContext";
 
-export const UserDataContext = createContext<User>(null);
+export const UserDataContext = createContext<[User, boolean]>(null);
 export function UserDataProvider({children}){
   const [user, setUser] = React.useState<User>(null);
+  const [loading, setLoading] = React.useState(true);
   const auth = React.useContext(FirebaseAuthContext);
 
   React.useEffect(() => {
-    if(!auth) return;
-    if(auth.currentUser) setUser(auth.currentUser);
-    onAuthStateChanged(auth, user => {
-      if(user) setUser(user);
-      else setUser(null);
-    })
+    const handleuser = async () => {
+      if(!auth) return;
+      await auth.authStateReady().then(() => {
+        if(auth.currentUser){
+          setUser(auth.currentUser);
+        }
+        setLoading(false);
+      });
+      onAuthStateChanged(auth, user => {
+        if(user) setUser(user);
+        else setUser(null);
+      })
+    }
+    handleuser();
   }, [auth]);
 
   return(
-    <UserDataContext.Provider value={user}>
-        {children}
+    <UserDataContext.Provider value={[user, loading]}>
+      {children}
     </UserDataContext.Provider>
   )
 }
