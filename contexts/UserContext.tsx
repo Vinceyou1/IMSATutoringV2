e5a347dfa033aa332ee92344289bd206d24cd032
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-08-07 14:19:15 -0500

    Added popup for non-IMSA email

--- contexts/UserContext.tsx
+++ contexts/UserContext.tsx
@@ -3,12 +3,15 @@ import { User, onAuthStateChanged } from "firebase/auth";
 import React from "react";
 import { createContext } from "react";
 import { FirebaseAuthContext } from "./FirebaseContext";
+import Popup from "reactjs-popup";
+import './popup.css'
 
 export const UserDataContext = createContext<[User, boolean]>(null);
 export function UserDataProvider({children}){
   const [user, setUser] = React.useState<User>(null);
   const [loading, setLoading] = React.useState(true);
   const auth = React.useContext(FirebaseAuthContext);
+  const [badEmail, updateBadEmail] = React.useState(false);
 
   React.useEffect(() => {
     const handleuser = async () => {
@@ -20,7 +23,15 @@ export function UserDataProvider({children}){
         setLoading(false);
       });
       onAuthStateChanged(auth, user => {
-        if(user) setUser(user);
+        if(user){
+          setUser(user);
+          if(user.email?.slice(user.email?.indexOf("@")) != "@imsa.edu"){
+            updateBadEmail(true)
+            setUser(null);
+            console.log("bad");
+            auth.signOut();
+          }
+        }
         else setUser(null);
       })
     }
@@ -29,6 +40,18 @@ export function UserDataProvider({children}){
 
   return(
     <UserDataContext.Provider value={[user, loading]}>
+      <Popup
+        open={badEmail}
+        modal
+        onClose={() => updateBadEmail(false)}
+        closeOnDocumentClick
+      >
+        <div className="modal flex flex-col justify-center bg-primary dark:bg-primary-dark border-2 rounded-lg">
+          <div className="text-center p-0 text-3xl">
+            Please Sign in With Your IMSA Email
+          </div>
+        </div>
+      </Popup>
       {children}
     </UserDataContext.Provider>
   )
