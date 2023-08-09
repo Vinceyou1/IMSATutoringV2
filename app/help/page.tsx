cbece51444c040bca8032292f560dd370c7a6965
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-08-09 16:29:13 -0500

    Update page.tsx

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Footer from "@/components/Footer";
 import { FirebaseFirestoreContext } from "@/contexts/FirebaseContext";
 import { addDoc, collection } from "firebase/firestore";
 import { useContext, useState } from "react";
@@ -35,24 +36,27 @@ export default function Help(){
     });
   }
   return (
-    <div className="flex flex-row items-center w-[90%] ml-auto mr-auto h-[calc(100%-10rem)] bg-cover bg-[url(/scattered-forcefields2.svg)] dark:bg-[url(/scattered-forcefields2-dark.svg)]">
-      <div className="w-[min(32rem,_100%)] mt-8 ml-auto mr-auto bg-primary dark:bg-primary-dark rounded-lg border-2 border-[rgb(203,_213,_224)] p-4">
-        <div className="mt-2">
-          <h3 className="text-center text-2xl mt-4">Contact Us!</h3>
-          <label id="label" className="text-lg" htmlFor="name">
-            Name:
-          </label>
-          <input type="textarea" id="name" name="name" required className="bg-[white] dark:bg-primary-dark p-2 w-full border-2 rounded-md border-[rgb(203,_213,_224)]"/>
+    <div className="flex flex-col w-full h-[calc(100%-5rem)] bg-cover bg-[url(/scattered-forcefields2.svg)] dark:bg-[url(/scattered-forcefields2-dark.svg)]">
+      <div className="w-[90%] ml-auto mr-auto flex flex-row flex-grow items-center">
+        <div className="w-[min(32rem,_90%)] mt-8 ml-auto mr-auto bg-primary dark:bg-primary-dark rounded-lg border-2 border-[rgb(203,_213,_224)] p-4">
+          <div className="mt-2">
+            <h3 className="text-center text-2xl mt-4">Contact Us!</h3>
+            <label id="label" className="text-lg" htmlFor="name">
+              Name:
+            </label>
+            <input type="textarea" id="name" name="name" required className="bg-[white] dark:bg-primary-dark p-2 w-full border-2 rounded-md border-[rgb(203,_213,_224)]"/>
+          </div>
+          <div className="mt-4">
+            <label id="label" className="text-lg" htmlFor="issue">
+              Describe the Issue:
+            </label>
+            <textarea id="issue" name = "issue" rows={4} className="resize-none bg-[white] dark:bg-primary-dark p-2 w-full border-2 rounded-md border-[rgb(203,_213,_224)]" required/>                                   
+          </div>
+          <p className={"text-[red] text-center " + (error_visible ? "mt-2 visible" : "h-0 invisible")}>You need to fill in all fields.</p>
+          <button onClick={() => {submitContact()}} id="submit" className="mt-4 h-12 text-primary dark:text-primary-dark duration-300 ease-in-out w-full rounded-md bg-[rgb(93,_170,_244)] hover:bg-[rgb(74,_132,_220)] font-bold">SUBMIT</button>
         </div>
-        <div className="mt-4">
-          <label id="label" className="text-lg" htmlFor="issue">
-            Describe the Issue:
-          </label>
-          <textarea id="issue" name = "issue" rows={4} className="resize-none bg-[white] dark:bg-primary-dark p-2 w-full border-2 rounded-md border-[rgb(203,_213,_224)]" required/>                                   
-        </div>
-        <p className={"text-[red] text-center " + (error_visible ? "mt-2 visible" : "h-0 invisible")}>You need to fill in all fields.</p>
-        <button onClick={() => {submitContact()}} id="submit" className="mt-4 h-12 text-primary dark:text-primary-dark duration-300 ease-in-out w-full rounded-md bg-[rgb(93,_170,_244)] hover:bg-[rgb(74,_132,_220)] font-bold">SUBMIT</button>
       </div>
+      <Footer />
     </div>
   )
 }
